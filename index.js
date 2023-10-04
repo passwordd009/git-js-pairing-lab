1abeebdf2c379d635efe786f4588cd684fe147da
nico-j3 <nicolejuarezflores@gmail.com>
2023-10-04 14:34:33 -0400

    problem 2

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 //Code your solutions in this file
+
+//problem 1
 const fiveToOneHundred = () => {
     for(let i = 5; i <= 100; i++){
         console.log(i);
     }
 }
 console.log(fiveToOneHundred());
+
+//problem 2
+const multiplesOfThree =() => {
+    for (let i = 1 ; i <=100; i++) {
+        x = i*3; 
+        console.log(x);
+        if (x===99) {
+            i=100; 
+        }
+    } 
+}
+console.log(multiplesOfThree())
